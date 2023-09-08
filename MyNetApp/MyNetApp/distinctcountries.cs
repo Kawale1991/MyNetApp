23bfa30213028c16c70111e27ae08e230de64582
Shyam <shyamkawale04@gmail.com>
2023-09-08 16:53:10 +0530

    New file added to find distict contries

---
+++ MyNetApp/MyNetApp/distinctcountries.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyNetApp
+{
+    internal class distinctcountries
+    {
+
+    }
+    public class Dcountries
+    {
+        public static void Main(string[] args)
+        {
+            List<string> countries = new List<string> {"India","Singapore","US", "India", "Singapore", "US" };
+            IEnumerable<string> distinctcountries = countries.Distinct();
+
+            foreach (var item in distinctcountries)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+        }
+    }
+}
